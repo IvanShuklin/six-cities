61a114cc0dfd09589dd53ed411ac4e091b79d6fd
IvanShuklin <157677279+IvanShuklin@users.noreply.github.com>
2026-01-10 15:49:52 +0300

    react-helmet для главной страницы

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import OfferCard from '../../components/offer-card/offer-card';
 import NavTabs from './components/nav-tabs/nav-tabs';
 import PlacesSorting from './components/places-sorting/places-sorting';
@@ -8,28 +9,34 @@ type MainPageProps = {
 
 export default function MainPage({offersAmount}: MainPageProps) {
   return (
-    <main className="page__main page__main--index">
-      <h1 className="visually-hidden">Cities</h1>
-      <NavTabs />
-      <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{offersAmount} places to stay in Amsterdam</b>
-            <PlacesSorting />
-            <div className="cities__places-list places__list tabs__content">
-              <OfferCard />
-              <OfferCard />
-              <OfferCard />
-              <OfferCard />
-              <OfferCard />
+    <>
+      <Helmet>
+        <title>6 Cities</title>
+      </Helmet>
+
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <NavTabs />
+        <div className="cities">
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">{offersAmount} places to stay in Amsterdam</b>
+              <PlacesSorting />
+              <div className="cities__places-list places__list tabs__content">
+                <OfferCard />
+                <OfferCard />
+                <OfferCard />
+                <OfferCard />
+                <OfferCard />
+              </div>
+            </section>
+            <div className="cities__right-section">
+              <section className="cities__map map" />
             </div>
-          </section>
-          <div className="cities__right-section">
-            <section className="cities__map map" />
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
