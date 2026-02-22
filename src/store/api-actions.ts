2d6b982c0ed9d4b55ef8866cc71729732b342a8a
IvanShuklin <157677279+IvanShuklin@users.noreply.github.com>
2026-02-22 22:38:22 +0300

    refactoring

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -3,6 +3,7 @@ import { AxiosInstance } from 'axios';
 import { APIRoute, AuthorizationStatus } from '../const/const';
 import { saveToken } from '../services/token';
 import { AuthData } from '../types/auth-data';
+import { UserData } from '../types/user-data';
 
 export const checkAuth = createAsyncThunk<
   AuthorizationStatus,
@@ -23,17 +24,21 @@ export const checkAuth = createAsyncThunk<
 export const login = createAsyncThunk<
   AuthorizationStatus,
   AuthData,
-  { extra: AxiosInstance }
+  { extra: AxiosInstance; rejectValue: string }
 >(
   'user/login',
-  async ({ login: email, password }, { extra: api }) => {
-    const { data } = await api.post<{ token: string }>(
-      APIRoute.Login,
-      { email, password }
-    );
+  async ({ email, password }, { extra: api, rejectWithValue }) => {
+    try {
+      const { data } = await api.post<UserData>(
+        APIRoute.Login,
+        { email, password }
+      );
 
-    saveToken(data.token);
+      saveToken(data.token);
 
-    return AuthorizationStatus.Auth;
+      return AuthorizationStatus.Auth;
+    } catch (error) {
+      return rejectWithValue('Login failed');
+    }
   }
 );
